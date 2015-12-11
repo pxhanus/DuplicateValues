368efbf3efc740c2e73a7a68a3a340825a4034db
pxhanus <pxhanus@gmail.com>
2015-12-11 14:22:59 -0600

    Initial commit

---
+++ DuplicateValues/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInputNumbers = New System.Windows.Forms.Button()
+        Me.lblDuplicate = New System.Windows.Forms.Label()
+        Me.lstValues = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnInputNumbers
+        '
+        Me.btnInputNumbers.Location = New System.Drawing.Point(12, 12)
+        Me.btnInputNumbers.Name = "btnInputNumbers"
+        Me.btnInputNumbers.Size = New System.Drawing.Size(85, 23)
+        Me.btnInputNumbers.TabIndex = 0
+        Me.btnInputNumbers.Text = "Input Numbers"
+        Me.btnInputNumbers.UseVisualStyleBackColor = True
+        '
+        'lblDuplicate
+        '
+        Me.lblDuplicate.Location = New System.Drawing.Point(103, 12)
+        Me.lblDuplicate.Name = "lblDuplicate"
+        Me.lblDuplicate.Size = New System.Drawing.Size(102, 36)
+        Me.lblDuplicate.TabIndex = 1
+        '
+        'lstValues
+        '
+        Me.lstValues.FormattingEnabled = True
+        Me.lstValues.Location = New System.Drawing.Point(12, 51)
+        Me.lstValues.Name = "lstValues"
+        Me.lstValues.Size = New System.Drawing.Size(193, 160)
+        Me.lstValues.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(217, 223)
+        Me.Controls.Add(Me.lstValues)
+        Me.Controls.Add(Me.lblDuplicate)
+        Me.Controls.Add(Me.btnInputNumbers)
+        Me.Name = "Form1"
+        Me.Text = "Duplicate Values"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnInputNumbers As System.Windows.Forms.Button
+    Friend WithEvents lblDuplicate As System.Windows.Forms.Label
+    Friend WithEvents lstValues As System.Windows.Forms.ListBox
+
+End Class
---
+++ DuplicateValues/Form1.vb
@@ -0,0 +1,65 @@
+﻿'Patricia Hanus
+'151211
+'Duplicate Values
+'Public Class Form1
+'____________________________________________________________________________________________
+Public Class Form1
+
+    Private Sub btnInputNumbers_Click(sender As Object, e As EventArgs) Handles btnInputNumbers.Click
+        Dim numbers(0) As Integer
+        Me.lstValues.Items.Clear()
+        Dim length As Integer = 0
+        Dim inputNum As Integer
+
+        inputNum = Val(InputBox("Please input a number from 1 to 99"))
+
+        Do While FindItemIndex(numbers, inputNum) = -1
+            numbers(length) = inputNum
+            length += 1
+            ReDim Preserve numbers(length)
+            inputNum = Val(InputBox("Please input a number from 1 to 99"))
+        Loop
+
+
+
+        Call DisplayArray(Me.lstValues, numbers, length, Me.lblDuplicate)
+
+    End Sub
+    '********************************************************************************************
+    'DisplayArray
+    'Displays the contents of the passed array in the passed list box values index
+    '
+    'post:  Array results displayed in list box 
+    '********************************************************************************************
+    Sub DisplayArray(ByRef lstBox As ListBox, ByRef array() As Integer, ByVal duplicateIndex As Integer, ByRef lblDuplicate As Label)
+        For i As Integer = 0 To array.Length - 2
+            lstBox.Items.Add(array(i))
+        Next
+        Me.lblDuplicate.Text = "Duplicate after " & duplicateIndexx & " numbers"
+    End Sub
+    'Returns the index of the first occurrence of searchItem in
+    'dataArray or -1 if searchItem not found.
+    '
+    'post: Index of the first occurrence of searchItem has been 
+    'returned, or -1 has been returned if searchItem not found.
+    '
+    Function FindItemIndex(ByRef dataArray() As Integer, _
+    ByVal searchItem As Integer) As Integer
+        'Empty array
+        If dataArray.Length = 0 Then
+            Return -1               'item not found
+        End If
+
+        'Find search item
+        Dim index As Integer = 0
+        Do While (dataArray(index) <> searchItem) _
+        And (index < dataArray.Length - 1)
+            index += 1
+        Loop
+        If dataArray(index) = searchItem Then
+            Return index            'item found
+        Else
+            Return -1               'item not found
+        End If
+    End Function
+End Class
